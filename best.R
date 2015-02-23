84d9731e9c4bbb28fec10cc0e0d1711b636c6777
acheronHades <git@grintsch.org>
2015-02-23 15:28:37 +0100

    result for best.R lacking alphabetical sorting of multiple results

--- best.R
+++ best.R
@@ -1,8 +1,31 @@
 best <- function(state, outcome) {
   ## Read outcome data
+  outcomeDF <- read.csv("outcome-of-care-measures.csv", colClasses="character")
+  outcomeDF[,11] <- as.numeric(outcomeDF[,11]) ## coerce heart attack feature to numeric
+  outcomeDF[,17] <- as.numeric(outcomeDF[,17]) ## coerce heart failure feature to numeric
+  outcomeDF[,23] <- as.numeric(outcomeDF[,23]) ## coerce pneumonia feature to numeric
   
   ## Check that state and outcome are valid
+  possibleoutcomes = c("heart failure", "heart attack", "pneumonia") ## list of possible outcomes to check input
+  if (state %in% outcomeDF$State == FALSE) 
+    stop("invalid state")
+  if (outcome %in% possibleoutcomes == FALSE)
+    stop("invalid outcome")
   
   ## Return hospital name in that state with lowest 30-day death rate
+  onestate <- outcomeDF[outcomeDF$State == state,]
+  minHA <- min(na.omit(onestate[,11]))
+  minHF <- min(na.omit(onestate[,17]))
+  minP <- min(na.omit(onestate[,23]))
   
+  
+  if (outcome == "heart attack")
+    result <- which(onestate[,11] == minHA)
+  if (outcome == "heart failure")
+    result <- which(onestate[,17] == minHF)
+  if (outcome == "pneumonia")
+    result <- which(onestate[,23] == minP)
+
+  name <- onestate$Hospital.Name[result]
+  print(name)
 }
